00ffc7d10f4ff93e9b39516e94aff1c9bf99d634
Ben Buzard <ben@benbuzard.com>
2023-07-26 15:50:33 -0700

    Set copyright to Apache-2.0

--- src/commonMain/kotlin/com/benbuzard/apilib/jsonschema/JsonSchemaNode.kt
+++ src/commonMain/kotlin/com/benbuzard/apilib/jsonschema/JsonSchemaNode.kt
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2023 Ben Buzard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.benbuzard.apilib.jsonschema
 
 import com.benbuzard.apilib.serializers.RegexAsString
-import kotlinx.serialization.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonElement
 import kotlin.Boolean
 import kotlin.String
@@ -10,9 +27,8 @@ import kotlin.reflect.KFunction
 import kotlin.reflect.KType
 import kotlin.reflect.full.isSubtypeOf
 import kotlin.reflect.full.starProjectedType
-
-import kotlin.String as KString
 import kotlin.Boolean as KBoolean
+import kotlin.String as KString
 
 // TODO: some of these fields are not entirely correct (some should have array options, some should have boolean options_
 // TODO: much of this needs to be verified against the spec
